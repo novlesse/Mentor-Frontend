9b1f68f52b0320eb4c9ceed8bc30c4033722b932
homer li <homer.lee.0508@gmail.com>
2021-01-10 03:27:56 -0800

    image render

--- src/components/MainSectionTwo.js
+++ src/components/MainSectionTwo.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { Jumbotron, Row, Col, Image } from 'react-bootstrap';
-import {MainSectionContainerTwo, ImageBox, SubtitleBox, TitleBoxTwo} from './elements'
+import React from "react";
+import { Jumbotron, Row, Col, Image } from "react-bootstrap";
+import {
+  MainSectionContainerTwo,
+  ImageBox,
+  SubtitleBox,
+  TitleBoxTwo,
+} from "./elements";
 
 const MainSectionTwo = (props) => {
-	return (
-		<MainSectionContainerTwo>
-			<Row>
-				<Col>
-                    <Col>
-					<Row className="d-flex justify-content-end">
-						<TitleBoxTwo>{props.title}</TitleBoxTwo>
-					</Row>
-					<Row className="d-flex justify-content-end">
-						<SubtitleBox>{props.subtitle}</SubtitleBox>
-					</Row>
-                    </Col>
-				</Col>
-				<Col>
-					<Image src={props.img} alt={props.alt}/>
-					
-				</Col>
-			</Row>
-		</MainSectionContainerTwo>
-	);
+  console.log(props.image);
+  return (
+    <MainSectionContainerTwo>
+      <Row>
+        <Col>
+          <Col>
+            <Row className="d-flex justify-content-end">
+              <TitleBoxTwo>{props.title}</TitleBoxTwo>
+            </Row>
+            <Row className="d-flex justify-content-end">
+              <SubtitleBox>{props.subtitle}</SubtitleBox>
+            </Row>
+          </Col>
+        </Col>
+        <Col>
+          <img src={props.image} alt={props.alt} width="80%" />
+        </Col>
+      </Row>
+    </MainSectionContainerTwo>
+  );
 };
 
 export default MainSectionTwo;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,56 @@
-import React from 'react';
-import { Jumbotron, Row, Col, Image, Container } from 'react-bootstrap';
-import Footer from '../components/Footer.js';
-import Hero from '../components/Hero.js';
-import MainSection from '../components/MainSection.js';
-import MainSectionTwo from '../components/MainSectionTwo.js';
-
+import React from "react";
+import { Jumbotron, Row, Col, Image, Container } from "react-bootstrap";
+import Footer from "../components/Footer.js";
+import Hero from "../components/Hero.js";
+import MainSection from "../components/MainSection.js";
+import MainSectionTwo from "../components/MainSectionTwo.js";
 
 const HomeSection = (props) => {
   const sectionOne = {
     title: "Want to learn something new?",
     subtitle: "Start Searching",
-    img: '../images/pottery.jpg',
-    alt: "pottery"
+    img: require("../images/pottery.jpg").default,
+    alt: "pottery",
   };
 
   const sectionTwo = {
     title: "Have a skill? Teach a class.",
     subtitle: "Register Now",
-    img: '../images/drawing.jpg',
-    alt: "art"
+    img: require("../images/drawing.jpg").default,
+    alt: "art",
   };
   const sectionThree = {
     title: "Have a space to rent out?",
     subtitle: "Email to let us know",
-    img: '../images/rentalroom.jpg',
-    alt: "empty space"
+    img: require("../images/rentalroom.jpg").default,
+    alt: "empty space",
   };
 
+  return (
+    <Container fluid>
+      <Hero />
+      <MainSection
+        title={sectionOne.title}
+        subtitle={sectionOne.subtitle}
+        image={sectionOne.img}
+        alt={sectionOne.alt}
+      />
+      <MainSectionTwo
+        title={sectionTwo.title}
+        subtitle={sectionTwo.subtitle}
+        image={sectionTwo.img}
+        alt={sectionTwo.alt}
+      />
+      <MainSection
+        title={sectionThree.title}
+        subtitle={sectionThree.subtitle}
+        image={sectionThree.img}
+        alt={sectionThree.alt}
+      />
 
-	return (
-		<Container fluid>
-			<Hero />
-			<MainSection title={sectionOne.title} subtitle={sectionOne.subtitle} image={sectionOne.img} alt={sectionOne.alt}/>
-			<MainSectionTwo title={sectionTwo.title} subtitle={sectionTwo.subtitle} image={sectionTwo.img} alt={sectionTwo.alt}/>
-			<MainSection title={sectionThree.title} subtitle={sectionThree.subtitle} image={sectionThree.img} alt={sectionThree.alt}/>
-
-			
       <Footer />
-		</Container>
-
-	);
+    </Container>
+  );
 };
 
 export default HomeSection;
