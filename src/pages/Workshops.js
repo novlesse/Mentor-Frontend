19a13f289cdaf3e53d32534de57c95991247a6be
Emmy <ewong96@live.ca>
2021-01-10 02:21:12 -0800

    workshop category page wip

--- src/pages/Workshops.js
+++ src/pages/Workshops.js
@@ -1,9 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import CategoryBox from '../components/CategoryBox';
 
 export default function Workshops(props) {
+
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     document.title = "Workshops";
+    Axios.get(`http://localhost:3333/category/all`)
+        .then((res)=>{
+            const allCategories = res.data;
+            allCategories.foreach(x => setCategories(categories => [...categories, x]))
+
+        })
   }, []);
   
-  return <div>Workshops</div>;
+  return (
+    <div>
+      {categories.map((category)=>
+          <CategoryBox key={category._id} name={category.name} description={category.description} tags={category.tags} />
+      )} 
+    </div>
+  
+  )
 }
